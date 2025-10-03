5cdf0c684308e2f7075214af6041c50d47e91f86
seyfo32 <75393169+seyfo32@users.noreply.github.com>
2025-10-04 00:41:51 +0300

    Add user registration with Identity and role seed (Admin/User)

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
@@ -15,6 +17,30 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
-        
+       protected override void OnModelCreating(ModelBuilder builder)
+{
+    base.OnModelCreating(builder);
+
+    List<IdentityRole> roles = new List<IdentityRole>
+    {
+        new IdentityRole
+        {
+            Id = "admin-role-id",
+            Name = "Admin",
+            NormalizedName = "ADMIN",
+            ConcurrencyStamp = "a1111111-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
+        },
+        new IdentityRole
+        {
+            Id = "user-role-id",
+            Name = "User",
+            NormalizedName = "USER",
+            ConcurrencyStamp = "b2222222-bbbb-bbbb-bbbb-bbbbbbbbbbbb"
+        }
+    };
+
+    builder.Entity<IdentityRole>().HasData(roles);
+}
+
     }
 }
