9f14b23e73fb5b538fffe963adfa6439d475faef
seyfo32 <75393169+seyfo32@users.noreply.github.com>
2025-10-07 00:50:44 +0300

    Add delete portfolio feature with controller and repository logic

--- Controller/PortfolioController.cs
+++ Controller/PortfolioController.cs
@@ -73,6 +73,30 @@ namespace api.Controller
             }
         }
 
+        [HttpDelete]
+        [Authorize]
+
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUserName();
+            var appUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower()).ToList();
+
+            if (filteredStock.Count() == 1)
+            {
+                await _portfolioRepo.DeletePortfolio(appUser, symbol);
+            }
+            else
+            {
+                return BadRequest("Stock not in your portfolio");
+            }
+            return NoContent();
+
+        }
+
 
     }
 }
